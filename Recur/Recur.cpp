02c1a927b7be39a038f41166504d98261cb49476
Bikmacs <macsim.shmelev@yandex.ru>
2024-11-24 01:17:56 +0500

    rec

---
+++ Recur/Recur.cpp
@@ -0,0 +1,40 @@
+﻿
+#include <iostream>
+using namespace std;
+
+bool equal(int N, int S) {
+
+    if (N == 0) {
+        return S == 0;
+    }
+
+    S -= N % 10;
+    N /= 10 ;
+    return equal(N, S);
+}
+
+int main()
+{
+    setlocale(0, "rus");
+    int N = 0;
+    int S = 0;
+    std::cout << "Введите N";
+    std::cin >> N;
+    std::cout << "Введите S";
+    std::cin >> S;
+    if (equal(N, S)) {
+        std::cout << "Сумма " << N << " равна " << S << std::endl;
+    }
+    else {
+        std::cout << "Сумма цифр числа " << N << " не равна " << S << std::endl;
+    }
+    return 0;
+}
+
+
+struct Pers
+{
+
+};
+
+
